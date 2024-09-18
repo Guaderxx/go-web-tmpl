0179548962a0982a8eba1a5b4ce29d32a5814631
guader <guaderxx@gmail.com>
2024-09-18 16:48:43 +0800

    feat(config): add db related config

--- config/config.go
+++ config/config.go
@@ -10,4 +10,14 @@ type Config struct {
 	AppName string `mapstructure:"app_name"`
 	Log     alog.Options
 	Web     web.Options
+	DB      DB
+}
+
+type DB struct {
+	Name     string
+	Host     string
+	Port     int
+	User     string
+	Schema   string
+	Password string
 }
