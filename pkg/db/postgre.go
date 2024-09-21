917147dce7b82577ff596c7b5bc24a5fe169e1e3
guader <guaderxx@gmail.com>
2024-09-21 21:51:14 +0800

    fix(ent/client): add auto migrate function

--- pkg/db/postgre.go
+++ pkg/db/postgre.go
@@ -1,13 +1,18 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 
 	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
 	"github.com/Guaderxx/gowebtmpl/ent"
+	"github.com/Guaderxx/gowebtmpl/pkg/alog"
 
+	// Import this to avoid import cycle error
+	// See https://entgo.io/docs/hooks#hooks-registration
+	_ "github.com/Guaderxx/gowebtmpl/ent/runtime"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -20,5 +25,14 @@ func Open(databaseUrl string) (*ent.Client, error) {
 
 	// Create an ent.Driver from `db`.
 	drv := entsql.OpenDB(dialect.Postgres, db)
-	return ent.NewClient(ent.Driver(drv)), nil
+	client := ent.NewClient(ent.Driver(drv))
+
+	AutoMigrate(client)
+	return client, nil
+}
+
+func AutoMigrate(db *ent.Client) {
+	if err := db.Schema.Create(context.Background()); err != nil {
+		alog.Fatal("create schema resources failed", "error", err.Error())
+	}
 }
