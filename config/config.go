99ff1d380a5b52d65f62cbd199e862623207aa03
guader <guaderxx@gmail.com>
2024-09-21 21:48:39 +0800

    fix(config): move web.Options to config
    - add jwt related config

--- config/config.go
+++ config/config.go
@@ -2,15 +2,15 @@ package config
 
 import (
 	"github.com/Guaderxx/gowebtmpl/pkg/alog"
-	"github.com/Guaderxx/gowebtmpl/pkg/web"
 )
 
 type Config struct {
 	Env     string
 	AppName string `mapstructure:"app_name"`
 	Log     alog.Options
-	Web     web.Options
+	Web     Web
 	DB      DB
+	JWT     JWT
 }
 
 type DB struct {
@@ -20,4 +20,19 @@ type DB struct {
 	User     string
 	Schema   string
 	Password string
+}
+
+type Web struct {
+	Port         string
+	ReadTimeout  int `mapstructure:"read_timeout"`
+	WriteTimeout int `mapstructure:"write_timeout"`
+	IdleTimeout  int `mapstructure:"idle_timeout"`
+	MaxHeaderMB  int `mapstructure:"max_header_mb"`
+}
+
+type JWT struct {
+	AccessTokenSecret      string `mapstructure:"access_token_secret"`
+	AccessTokenExpiryHour  int    `mapstructure:"access_token_expiry_hour"`
+	RefreshTokenSecret     string `mapstructure:"refresh_token_secret"`
+	RefreshTokenExpiryHour int    `mapstructure:"refresh_token_expiry_hour"`
 }
